fb9fd323a9b5535092eea542720add558ad97476
CaMeLCa5e <jm273606@gmail.com>
2014-10-11 10:51:17 -0400

    Changed to if loop, fixed spacing on print statement

--- cubedrootguessing.py
+++ cubedrootguessing.py
@@ -1,12 +1,20 @@
 #finding a cubed root by guess and check 
 
 x = int(raw_input("Enter an integer"))
-ans = 0
-while ans**3 < abs(x):
-	ans = ans +1  #Iterloop 
+
+#changed to if loop.
+for ans in range(0, abs(x)+1):
+	if ans**3 == abs(x):
+		break
+
+	#ans = 0
+	#while ans**3 < abs(x):
+	#	ans = ans +1  #Iterloop 
+
+
 if ans**3!=abs(x):
 	print(str(x)+ "is not a perfect cube")
 else:
 	if x < 0:
 		ans = -ans
-	print('Cube root of' + str(x)+ "is" + str(ans))
+	print('Cube root of' + str(x)+ " is " + str(ans))
